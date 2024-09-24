f37fd8b8e4e6c18aaa89586e48650518f6645ebf
s33h0w <theo.s33h0w@gmail.com>
2024-09-24 17:11:39 +0800

    Fix deploy error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,21 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://s33h0w.github.io",
-	integrations: [
-		mdx(),
-		sitemap({
-			changefreq: "daily",
-			priority: 1,
-			lastmod: new Date(),
-		}),
-		tailwind(),
-	],
-	output: "server",
-	adapter: cloudflare(),
+    site: "https://s33h0w.github.io",
+    integrations: [
+        mdx(),
+        sitemap({
+            changefreq: "daily",
+            priority: 1,
+            lastmod: new Date(),
+        }),
+        tailwind(),
+    ],
+    output: "server",
+    adapter: netlify(),
 });
